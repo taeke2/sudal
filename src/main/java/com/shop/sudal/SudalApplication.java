8974d39dce6bc82c4b98a562b9008d704f2c450a
taeke2 <62018298+taeke2@users.noreply.github.com>
2024-07-15 18:38:56 +0900

    Initial commit

---
+++ src/main/java/com/shop/sudal/SudalApplication.java
@@ -0,0 +1,13 @@
+package com.shop.sudal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/sudal/SudalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.sudal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
